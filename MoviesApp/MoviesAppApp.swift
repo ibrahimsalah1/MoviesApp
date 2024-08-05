e0914dff7cda0395684c59e8be366e74690e973b
Ibrahim Salah <ibrahimsalah093@gmail.com>
2024-08-05 18:59:53 +0300

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Ibrahim Salah on 05/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
